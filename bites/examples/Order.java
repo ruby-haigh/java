2def4327f6a4a8cb3699aa3dde22ecae6248c8fe
ruby-haigh <rubyimogen.h@gmail.com>
2026-02-23 16:02:43 +0000

    Update order with string manipulation

--- bites/examples/Order.java
+++ bites/examples/Order.java
@@ -14,10 +14,18 @@ public class Order {
     public void addDiscount(Integer discount) {
         this.total -= discount;
     }
+
+    private String friendlyTotal() {
+        return String.format("Thanks! The total value of your order is £%d", this.total);
+    }
+
     public static void main(String[] args) {
         Order myOrder = new Order();
         myOrder.addAmount(20);
         myOrder.addDiscount(2);
-        System.out.println("£" + myOrder.getTotal());
+        Integer total = myOrder.getTotal();
+        String stringTotal = myOrder.friendlyTotal();
+        System.out.println("£" + total);
+        System.out.println(stringTotal);
     }
 }
